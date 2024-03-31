6ef58e36fc0fbb2b9be310a9898944cdf6ed0379
johel360CR <98366214+johel360CR@users.noreply.github.com>
2024-03-31 12:55:06 -0600

    Update controles-formularios-2-02-1.php
    actualizacion de edad minima y peso minimo

--- controles-formularios-2-02-1.php
+++ controles-formularios-2-02-1.php
@@ -15,9 +15,9 @@
   <h1>Datos personales 2 (Formulario)</h1>
 
   <form action="controles-formularios-2-02-2.php" method="get">
-    <p><label>Escriba su edad: <input type="number" name="edad" min="5" max="130"></label></p>
+    <p><label>Escriba su edad: <input type="number" name="edad" min="1" max="110"></label></p>
 
-    <p><label>Escriba su peso: <input type="number" name="peso" step="0.1" min="10" max="150"></label></p>
+    <p><label>Escriba su peso: <input type="number" name="peso" step="0.1" min="2" max="150"></label></p>
 
     <p>
       <input type="submit" value="Enviar">
